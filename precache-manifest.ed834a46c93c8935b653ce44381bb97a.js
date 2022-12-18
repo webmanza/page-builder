ed4d8ab00d5aef397fdf4daa89946eb81c10959e
Forid Pathan <foridpathan45@gmail.com>
2022-12-18 10:27:46 +0600

    Updates

---
+++ precache-manifest.ed834a46c93c8935b653ce44381bb97a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85248369f1d924297959ce69c428ffb1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c998da6c9c4f40680392",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "dd4750f946766c8a602f",
+    "url": "./static/js/2.e280a003.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e280a003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c998da6c9c4f40680392",
+    "url": "./static/js/main.63540d14.chunk.js"
+  },
+  {
+    "revision": "c9b1a5e018dab40a12d8",
+    "url": "./static/js/runtime-main.2db1c156.js"
+  }
+]);
